b80a2c64d8268bd21204d829b4950b09fc38eae8
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-06-11 10:50:40 +0200

    fix: gcp tracing (#13)

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,10 +2,15 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
+
+const provider = new NodeTracerProvider();
+const exporter = new TraceExporter();
 
 export function initTracing() {
-  const provider = new NodeTracerProvider();
   provider.register();
+  provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
   registerInstrumentations({
     instrumentations: [new ExpressInstrumentation(), new HttpInstrumentation()],
