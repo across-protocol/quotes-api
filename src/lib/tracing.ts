d8c4ce4c667ced29041cfe92829e65a8f5e6aca6
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-06-11 14:30:15 +0200

    fix: use http instrumentation for ignoring health check trace (#16)

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,54 +2,27 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { ExpressInstrumentation } from "@opentelemetry/instrumentation-express";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import {
-  BatchSpanProcessor,
-  AlwaysOnSampler,
-  Sampler,
-  SamplingDecision,
-} from "@opentelemetry/sdk-trace-base";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
-import { SpanKind, Attributes } from "@opentelemetry/api";
-import { SEMATTRS_HTTP_ROUTE } from "@opentelemetry/semantic-conventions";
 
 export function initTracing() {
-  const provider = new NodeTracerProvider({
-    sampler: filterSampler(ignoreHealthCheck, new AlwaysOnSampler()),
-  });
+  const provider = new NodeTracerProvider();
   const exporter = new TraceExporter();
 
   provider.register();
   provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
   registerInstrumentations({
-    instrumentations: [new ExpressInstrumentation(), new HttpInstrumentation()],
+    instrumentations: [
+      new HttpInstrumentation({
+        ignoreIncomingRequestHook(req) {
+          if (req.url.match("/health")) {
+            return true;
+          }
+          return false;
+        },
+      }),
+      new ExpressInstrumentation(),
+    ],
   });
-}
-
-function filterSampler(
-  filterFn: (spanName: string, spanKind: SpanKind, attr: Attributes) => boolean,
-  parent: Sampler,
-): Sampler {
-  return {
-    shouldSample(ctx, tid, spanName, spanKind, attr, links) {
-      if (!filterFn(spanName, spanKind, attr)) {
-        return { decision: SamplingDecision.NOT_RECORD };
-      }
-      return parent.shouldSample(ctx, tid, spanName, spanKind, attr, links);
-    },
-    toString() {
-      return `FilterSampler(${parent.toString()})`;
-    },
-  };
-}
-
-function ignoreHealthCheck(
-  spanName: string,
-  spanKind: SpanKind,
-  attributes: Attributes,
-) {
-  return (
-    spanKind !== SpanKind.SERVER ||
-    attributes[SEMATTRS_HTTP_ROUTE] !== "/health"
-  );
 }
